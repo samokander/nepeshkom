7e49a65f07a16df4cfd578d21b9e082c1b88164c
SvyatS <prisenko2002@gmail.com>
2023-05-04 19:33:11 +0700

    feat: remove forest admin

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
-import { createAgent } from '@forestadmin/agent';
-import { createSqlDataSource } from '@forestadmin/datasource-sql';
-import 'dotenv/config';
 
 async function bootstrap() {
-  const agent = createAgent({
-    authSecret: process.env.FOREST_AUTH_SECRET,
-    envSecret: process.env.FOREST_ENV_SECRET,
-    isProduction: process.env.NODE_ENV === 'production',
-    typingsPath: './typings.ts',
-    typingsMaxDepth: 5,
-  }).addDataSource(createSqlDataSource(process.env.DATABASE_URL));
-
   const app = await NestFactory.create(AppModule);
 
   app.enableVersioning({
@@ -29,7 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
 
-  await agent.mountOnNestJs(app).start();
   await app.listen(3000);
 }
 bootstrap();
