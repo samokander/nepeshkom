6d1348276ea0cc6cb119f05971de253ad2b72b3a
SvyatS <prisenko2002@gmail.com>
2023-05-19 06:40:05 +0700

    feat: add auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpCode, Injectable } from '@nestjs/common';
 import { SMSRu } from 'node-sms-ru';
 import { ClientsService } from 'src/clients/clients.service';
 import { firstValueFrom } from 'rxjs';
@@ -38,20 +38,36 @@ export class AuthService {
     const verificationCode = Math.floor(Math.random() * 100000).toString();
     await this.clientRedis.set(phoneNumber, verificationCode);
 
-    return this.smsRu.sendSms({
+    const smsRes = await this.smsRu.sendSms({
       to: phoneNumber,
       msg: verificationCode,
     });
+
+    console.log(smsRes);
+
+    smsRes?.status === 'OK' ? HttpCode(200) : HttpCode(500);
+    return { status: smsRes?.status === 'OK' }
   }
 
   async checkVerificationCode({ phoneNumber, verificationCode }) {
-    const redisVerificationCode = this.clientRedis.get(phoneNumber);
+    await this.clientRedis.set(phoneNumber, '12345');
+    const redisVerificationCode = await this.clientRedis.get(phoneNumber);
+    let status = false;
+    let data = {};
 
-    if (redisVerificationCode === verificationCode) {
-      await this.clientRedis.del(phoneNumber);
-      return true;
-    } else {
-      return false;
+    if (redisVerificationCode !== verificationCode) return { status, data };
+    await this.clientRedis.del(phoneNumber);
+    const clients = await firstValueFrom(
+      await this.clientsService.getAllClients({
+        phoneNumber,
+      }),
+    );
+
+    if (clients?.Clients?.[0]) {
+      status = true;
+      data = clients?.Clients?.[0]
     }
+
+    return { status, data };
   }
 }
--- src/autos/dto/search-auto-input.dto.ts
+++ src/autos/dto/search-auto-input.dto.ts
@@ -1,31 +1,31 @@
 export class SearchAutoInputDto {
-    //  строка в формате:
-    //  "YYYY.MM.DD HO:MI:SE"
-    readonly dateFrom?: Date | string;
-    readonly dateTo?: Date | string;
-    //список кодов классов ТС
-    readonly autoClasses?: Array<string>;
-    readonly filialId?: number;
-    readonly brands?: Array<string>;
-    readonly colors?: Array<string>;
-    readonly transmissions?: Array<string>;
-    readonly bodyTypes?: Array<string>;
-    readonly privodTypes?: Array<string>;
-    // флаг - отобразить ТС, только с ВЫКЛЮЧЕННЫМ флагом "Ожадает продления"
-    readonly expectedProlongation?: boolean;
-    readonly viewInDay?: boolean;
-    //отобразить ТС у сотрудников
-    readonly viewMovements?: boolean;
-    // отобразить ТС на ремонта
-    readonly viewRepairs?: boolean;
-    //отобразить ТС без заявок на указанные даты
-    readonly cleanRequestReservs?: boolean;
-    //базовая цена ОТ (применятся в фильтре если > 0)
-	readonly defaultPriceFrom?: number;
-	//базовая цена ДО (применятся в фильтре если > 0) 
-	readonly defaultPriceTo?: number;
-	//мощность двигателя ОТ (применятся в фильтре если > 0)
-	readonly powerLSFrom?: number;
-	//мощность двигателя ДО (применятся в фильтре если > 0)
-	readonly powerLsTo?: number;
+  //  строка в формате:
+  //  "YYYY.MM.DD HO:MI:SE"
+  readonly dateFrom?: Date | string;
+  readonly dateTo?: Date | string;
+  //список кодов классов ТС
+  readonly autoClasses?: Array<string>;
+  readonly filialId?: number;
+  readonly brands?: Array<string>;
+  readonly colors?: Array<string>;
+  readonly transmissions?: Array<string>;
+  readonly bodyTypes?: Array<string>;
+  readonly privodTypes?: Array<string>;
+  // флаг - отобразить ТС, только с ВЫКЛЮЧЕННЫМ флагом "Ожадает продления"
+  readonly expectedProlongation?: boolean;
+  readonly viewInDay?: boolean;
+  //отобразить ТС у сотрудников
+  readonly viewMovements?: boolean;
+  // отобразить ТС на ремонта
+  readonly viewRepairs?: boolean;
+  //отобразить ТС без заявок на указанные даты
+  readonly cleanRequestReservs?: boolean;
+  //базовая цена ОТ (применятся в фильтре если > 0)
+  readonly defaultPriceFrom?: number;
+  //базовая цена ДО (применятся в фильтре если > 0)
+  readonly defaultPriceTo?: number;
+  //мощность двигателя ОТ (применятся в фильтре если > 0)
+  readonly powerLSFrom?: number;
+  //мощность двигателя ДО (применятся в фильтре если > 0)
+  readonly powerLsTo?: number;
 }
